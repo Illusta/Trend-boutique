068caab7a8bb7a995c84e5826a161410933b46a4
Illusta <97771516+Illusta@users.noreply.github.com>
2024-06-13 13:53:19 +0530

    Add files via upload

--- Trend.js
+++ Trend.js
@@ -1,3 +1,27 @@
 function myFunction(x) {
 x.classList.toggle("icon");
 }
+
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.banner-slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 3500); // Change slide every 5 seconds
+
+// Initial display
+showSlide(currentIndex);
