df2abefe879b87430c30547c448bbb42a5a3b270
Illusta <97771516+Illusta@users.noreply.github.com>
2024-08-20 22:06:57 +0530

    Add files via upload

--- Order.js
+++ Order.js
@@ -1,3 +1,12 @@
 function myFunction(x) {
 x.classList.toggle("icon");
 }
+
+function toggleOptions() {
+  var options = document.getElementById("options");
+  if (options.style.maxHeight) {
+    options.style.maxHeight = null;
+  } else {
+    options.style.maxHeight = options.scrollHeight + "px";
+  }
+}
